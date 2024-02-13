2eaface479509d120d8f37756d34610ae1800a9a
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-13 16:54:22 +0100

    added bootstrap frontend integration, with fixed navbar

--- index.php
+++ index.php
@@ -4,40 +4,73 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>Ajout produit</title>
     </head>
 
     <body>
-        <h1>Ajouter un produit</h1>
+        <header>
+            <!-- NAVBAR -->
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5" aria-label="Third navbar example">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Appli PHP</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-        <form action="traitement.php" method="post">
+                    <div class="collapse navbar-collapse" id="navbarsExample03">
+                        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Produits</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="recap.php">Panier</a>
+                            </li>
+                        </ul>
+                        
+                        <span class="badge bg-secondary">
+                            <?php 
+                                //$nbItems = array_key_last($_SESSION["products"]);
+                                //echo $nbItems;
+                            ?>
+                        </span>
+                    </div>
+                </div>
+            </nav>
+        </header>
 
-            <p>
-                <label>
-                    Nom du produit :
-                    <input type="text" name="name">
-                </label>
-            </p>
+        <main>
+            <div class="container mx-auto bg-light border">
+                <div class="container mt-3 pb-5">
+                    <h1>Ajouter un produit</h1>
+                </div>
 
-            <p>
-                <label>
-                    Prix du produit :
-                    <input type="number" step="any" name="price">
-                </label>
-            </p>
+                <div class="container px-5">
+                    <form action="traitement.php" method="post">
+                        <div class="mb-3">
+                            <label for="nom" class="form-label">Nom du produit : </label>
+                            <input type="text" class="form-control" id="nom" name="name" aria-describedby="emailHelp">
+                        </div>
 
-            <p>
-                <label>
-                    Quantité du produit :
-                    <input type="number" name="qtt" value="1">
-                </label>
-            </p>
+                        <div class="mb-3">
+                            <label for="prix" class="form-label">Prix du produit : </label>
+                            <input type="number" class="form-control" id="prix" name="price">
+                        </div>
 
-            <p>
-                <input type="submit" name="submit" value="Ajouter le produit">
-            </p>
-
-        </form>
+                        <div class="mb-5">
+                            <label for="quantite" class="form-label">Quantité d'articles : </label>
+                            <input type="number" class="form-control" id="quantite" name="qtt">
+                        </div>
+                       
+                        <div class="mb-4">
+                            <input type="submit" name="submit" value="Ajouter le produit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 
 </html>
--- recap.php
+++ recap.php
@@ -8,49 +8,87 @@ session_start();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>Recapitulatif des produits</title>
     </head>
 
     <body>
-        <?php
-            //IF products array is not set (session not started) OR products array is empty, display message.
-            if(!isset($_SESSION["products"]) || empty($_SESSION["products"])){
-                echo "<p>Aucun produit en session ...</p>";
-            }
-            //ELSE display table containing array elements.
-            else{
-                echo "<table>",
-                        "<thead>",
-                            "<tr>",
-                                "<th>#</th>",
-                                "<th>Nom</th>",
-                                "<th>Prix</th>",
-                                "<th>Quantité</th>",
-                                "<th>Total</th>",
+        <header>
+            <!-- NAVBAR -->
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5" aria-label="Third navbar example">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Appli PHP</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarsExample03">
+                        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Produits</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="recap.php">Panier</a>
+                            </li>
+                        </ul>
+                        <form>
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+            <div class="container mx-auto bg-light border">
+                <div class="container mt-3 pb-5">
+                    <h2>Votre panier contient : </h2>
+                </div>
+
+                <?php
+                    //IF products array is not set (session not started) OR products array is empty, display message.
+                    if(!isset($_SESSION["products"]) || empty($_SESSION["products"])){
+                        echo "<p>Aucun produit en session ...</p>";
+                    }
+                    //ELSE display table containing array elements.
+                    else{
+                        echo "<table class='table'>",
+                                "<thead>",
+                                    "<tr>",
+                                        "<th scope='col'>#</th>",
+                                        "<th scope='col'>Nom</th>",
+                                        "<th scope='col'>Prix</th>",
+                                        "<th scope='col'>Quantité</th>",
+                                        "<th scope='col'>Total</th>",
+                                    "</tr>",
+                                "</thead>",
+
+                                "<tbody>";
+                        //total price of all products
+                        $totalGeneral = 0;
+
+                        foreach($_SESSION["products"] as $index => $product){
+                            echo "<tr>",
+                                    "<td>",$index+1,"</td>",
+                                    "<td>".$product['name']."</td>",
+                                    "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                                    "<td>".$product['qtt']."</td>",
+                                    "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                                "</tr>";
+                            $totalGeneral += $product['total'];
+                        }
+                        echo "<tr>",
+                                "<td colspan=4>Total général : </td>",
+                                "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
                             "</tr>",
-                        "</thead>",
+                            "</tbody",
+                            "</table>";
+                    }
+                ?>
 
-                        "<tbody>";
-                //total price of all products
-                $totalGeneral = 0;
+            </div>
+        </main>
 
-                foreach($_SESSION["products"] as $index => $product){
-                    echo "<tr>",
-                            "<td>".$index."</td>",
-                            "<td>".$product['name']."</td>",
-                            "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                            "<td>".$product['qtt']."</td>",
-                            "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                        "</tr>";
-                    $totalGeneral += $product['total'];
-                }
-                echo "<tr>",
-                        "<td colspan=4>Total général : </td>",
-                        "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
-                    "</tr>",
-                    "</tbody",
-                    "</table>";
-            }
-        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
