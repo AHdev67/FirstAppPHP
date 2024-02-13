579c6d139fee3acd47327aef468120bb6ce26586
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-13 11:19:07 +0100

    added table display for all saved products, with failure condition

---
+++ recap.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Recapitulatif des produits</title>
+    </head>
+
+    <body>
+        <?php
+            //IF products array is not set (session not started) OR products array is empty, display message.
+            if(!isset($_SESSION["products"]) || empty($_SESSION["products"])){
+                echo "<p>Aucun produit en session ...</p>";
+            }
+            //ELSE display table containing array elements.
+            else{
+                echo "<table>",
+                        "<thead>",
+                            "<tr>",
+                                "<th>#</th>",
+                                "<th>Nom</th>",
+                                "<th>Prix</th>",
+                                "<th>Quantité</th>",
+                                "<th>Total</th>",
+                            "</tr>",
+                        "</thead>",
+
+                        "<tbody>";
+                //total price of all products
+                $totalGeneral = 0;
+
+                foreach($_SESSION["products"] as $index => $product){
+                    echo "<tr>",
+                            "<td>".$index."</td>",
+                            "<td>".$product['name']."</td>",
+                            "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                            "<td>".$product['qtt']."</td>",
+                            "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                        "</tr>";
+                    $totalGeneral += $product['total'];
+                }
+                echo "<tr>",
+                        "<td colspan=4>Total général : </td>",
+                        "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                    "</tr>",
+                    "</tbody",
+                    "</table>";
+            }
+        ?>
+    </body>
+</html>
