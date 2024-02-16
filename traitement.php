c2f5638007cb0fe555f77e3dc62b5d4da91b7092
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-16 13:52:24 +0100

    updated switch for button functions, added message attributions

--- traitement.php
+++ traitement.php
@@ -2,43 +2,89 @@
 
 session_start();
 
-if (isset($_POST['submit'])){
-    //excludes special characters and thus prevents html code injection.
-    $name = filter_input(INPUT_POST,"name", FILTER_SANITIZE_STRING);
-    //guarentees that the input is a decimal number, and allows both . AND , to express the decimal.
-    $price = filter_input(INPUT_POST,"price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    //guarentees that the input is an integer different than 0.
-    $qtt = filter_input(INPUT_POST,"qtt", FILTER_VALIDATE_INT);
+if( isset($_GET['action'])){
 
-    //IF all form elements have been filled correctly upon submit, fill product array.
-    if($name && $price && $qtt){
-        $product = [
-            "name"=> $name,
-            "price"=> $price,
-            "qtt"=> $qtt,
-            "total"=> $price*$qtt
-        ];
-        //fill products array with product arrays (i know) to display the total list of saved products.
-        $_SESSION['products'][] = $product;  
-    }
-    
-    header("Location:index.php?success=1");
-    exit();
+    switch ($_GET['action']) {
+
+        //ADDING NEW PRODUCT
+        case "add":
+            if(isset($_POST['add'])) {
+                //excludes special characters and thus prevents html code injection.
+                $name = filter_input(INPUT_POST,"name", FILTER_SANITIZE_STRING);
+                //guarentees that the input is a decimal number, and allows both . AND , to express the decimal.
+                $price = filter_input(INPUT_POST,"price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                //guarentees that the input is an integer different than 0.
+                $qtt = filter_input(INPUT_POST,"qtt", FILTER_VALIDATE_INT);
+
+                //IF all form elements have been filled correctly upon submit, fill product array.
+                if($name && $price && $qtt){
+                    $product = [
+                        "name"=> $name,
+                        "price"=> $price,
+                        "qtt"=> $qtt,
+                        "total"=> $price*$qtt
+                    ];
+                    //fill products array with product arrays (i know) to display the total list of saved products.
+                    $_SESSION['products'][] = $product;
+                    $_SESSION['alerte'] = "Produit ajouté !";
+                }
+                else{
+                    $_SESSION['alerte'] = "Erreur, aucun produit n'as été ajouté.";
+                }
+            }
+            header("Location:index.php?success=1");
+            break;
+
+        //INCREASE QUANTITY BY ONE
+        case 'upqtt':
+            if($_GET['action'] == "upqtt") {
+               
+                $index = isset($_GET['id']) ? $_GET['id'] : null;
+                if(isset($_SESSION['products'][$index]['qtt'])) {
+                    //incrémentation
+                    $_SESSION['products'][$index]['qtt']++;
+                    //MaJ prix
+                    $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['price'] * $_SESSION['products'][$index]['qtt'];
+                }
+            }
+            header("Location:recap.php");
+            break;
+
+        //DECREASE QUANTITY BY ONE
+        case 'downqtt':
+            if($_GET['action'] == "downqtt") {
+
+                $index = isset($_GET['id']) ? $_GET['id'] : null;
+                if(isset($_SESSION['products'][$index]['qtt']) && $_SESSION['products'][$index]['qtt'] >= 1) {
+                    //désincrémentation
+                    $_SESSION['products'][$index]['qtt']--;
+                    //MaJ prix
+                    $_SESSION['products'][$index]['total'] = $_SESSION['products'][$index]['price'] * $_SESSION['products'][$index]['qtt'];
+                }
+            }
+            header("Location:recap.php");
+            break;
+
+        //DELETE ITEM FROM CART
+        case 'delete' :
+            if($_GET['action'] == "delete") {
+
+                $index = $_GET['id'];
+
+                if(isset($_SESSION['products'][$index])) {
+                    unset($_SESSION['products'][$index]);
+                }
+            }
+            header("Location:recap.php");
+            break;
+
+        //CLEAR CART
+        case 'clear':
+            if($_GET['action'] == "clear") {
+
+                unset($_SESSION['products']);
+            }
+            header("Location:recap.php");
+            break;
+    }   
 }
-
-
-switch ($_GET["action"]) {
-
-    case 'upqtt':
-        //var_dump($_GET['id']);die();
-        //si il y a un produit en session avec le meme id alors je fait qtt ++*
-        $product['qtt'] += 1;
-        break;
-
-    case 'downqtt':
-        $product['qtt'] -= 1;
-        break;
-}
-
-header("Location:recap.php");
-exit();
