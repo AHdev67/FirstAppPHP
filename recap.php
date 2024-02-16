df79c6b5e34fb12593bbaa6f5c51828547bea1e9
AHdev67 <antoinehelfer.contact@gmail.com>
2024-02-16 14:52:03 +0100

    Adapted page for output buffer implementation, added dynamic title script

--- recap.php
+++ recap.php
@@ -1,19 +1,9 @@
 <?php
     session_start();
+    ob_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://kit.fontawesome.com/b28c0a82b5.js" crossorigin="anonymous"></script>
-        <title>Recapitulatif des produits</title>
-    </head>
-
-    <body>
+<!-- CONTENT -->
         <header>
             <!-- NAVBAR -->
             <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5" aria-label="Third navbar example">
@@ -120,6 +110,13 @@
             </div>
         </main>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
-</html>
+<script>
+    if (document.title != "Recap") {
+        document.title = "Recap";
+            }
+</script>
+
+<?php
+    $content = ob_get_clean();
+    require_once "template.php";
+?>
